9ff925ca0bfad3c27446a25c7ae1be8a2f839b67
rad <me@radnotred.dev>
2023-09-13 19:06:30 -0400

    fix: Button.tsx handleClick

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,21 @@
-import { CustomButtonProps } from '../utils'
-import { ShareIcon } from './icons'
-const Button = ({title, containerStyles, handleClick, textStyles, isDisabled, share }: CustomButtonProps) => {
-  return (
-    <button disabled={isDisabled} type={`button`} className={`custom-btn ${containerStyles}`} onClick={handleClick}>
-      <span className={`button-text ${textStyles}`}>
-        {title}
-      </span>
-      {share ? 
-        <ShareIcon/>
-      : null}
-    </button>
-  )
-}  
-export default Button
+import {CustomButtonProps} from '../utils';
+import {ShareIcon} from './icons';
+
+const Button = ({title, containerStyles, link, textStyles, isDisabled, share}: CustomButtonProps) => {
+    const handleClick = () => {
+        if (link) {
+            window.open(link, '_blank');
+        }
+    };
+
+    return (
+        <button disabled={isDisabled} type="button" className={`custom-btn ${containerStyles}`} onClick={handleClick}>
+            <span className={`button-text ${textStyles}`}>
+                {title}
+            </span>
+            {share ? <ShareIcon/> : null}
+        </button>
+    );
+};
+
+export default Button;
