d78af1b3c3a748b7bbfba48946895bd52abdd475
Mike Odnis <95197809+WomB0ComB0@users.noreply.github.com>
2023-12-23 23:48:43 -0500

    Quick commit

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { NextUIProvider } from '@nextui-org/react';
 import { AnimatePresence } from "framer-motion"
 import App from '@/app/App';
+import reportWebVitals from './reportWebVitals';
 import '@/styles/css/index.css'
 import '@/styles/global/globals.css'
 import '@/styles/global/globals.scss'
@@ -16,7 +17,7 @@ export const RootLayout = ({children}: {children: React.ReactNode}) =>  {
   )
 }
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')! as HTMLElement).render(
   <React.StrictMode>
     <NextUIProvider>
       <AnimatePresence>
@@ -29,3 +30,5 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     </NextUIProvider>
   </React.StrictMode>,
 );
+
+reportWebVitals()
