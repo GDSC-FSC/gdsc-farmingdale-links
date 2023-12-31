2c43b0edeb8cdac8a6fea175a1dd8026a2d617c8
Mike Odnis <airwolf635@gmail.com>
2023-12-31 16:14:13 -0500

    chore: minor local machine changes (different device)

--- src/hooks/useHasMounted.ts
+++ src/hooks/useHasMounted.ts
@@ -1,6 +1,21 @@
 import { useEffect, useState } from 'react';
-export default function useHasMounted() {
+
+export default function useHasMounted({ loading, time }: { loading?: boolean } & { time: number }) {
     const [hasMounted, setHasMounted] = useState(false);
-    useEffect(() => setHasMounted(true), []);
+    
+    useEffect(() => {
+        const originalTitle = document.title;
+
+        const handleContentLoaded = () => {
+            setTimeout(() => {
+                setHasMounted(true)
+            }, time);
+        }
+
+        if (loading) {
+        } else {
+            setHasMounted(true)
+        }
+    }, []);
     return hasMounted;
 }
